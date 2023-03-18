6228bfd036cfb1154b9f27c6743d65a3ed1d22cb
Wendy beekhuizen <wendybeekhuizen@hotmail.com>
2023-03-18 15:29:03 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,22 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+
+aanbieding = prijzen["vanille"] * 0.8
+
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+
+reclame_tekst2 = reclame_tekst[:63]
+
+reclame_tekst3 = reclame_tekst2.upper()
+
+reclame_tekst4 = reclame_tekst3.split(" ")
+
+for e1 in reclame_tekst4:
+    if len(e1) >= 5:
+        print(e1.upper())
+    else:
+        print(e1.lower())
+    
