3f527e51526e99afb3db61aa00ab9a091c21cfde
Wendy beekhuizen <wendybeekhuizen@hotmail.com>
2023-03-20 19:28:33 +0100

    laatste versie, les 10

--- presentatie.py
+++ presentatie.py
@@ -3,11 +3,3 @@ def presenteer(dictionary_2,totaal_bedrag):
         print(f"{k} : {v} euro")
     print(28 * "=")
     print(f"totaal : {totaal_bedrag} euro")
-   
-mijn_dict = {
-    'vis' : 10, 
-    'vlees' : 25, 
-    'overig' : 15
-    }
-
-totaal = 50
